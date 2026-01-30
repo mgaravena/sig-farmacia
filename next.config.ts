484099e37218d001086568b54ff3f93ced4016e0
Marcelo Aravena <Marcelo@MacBook-Air-de-Marcelo.local>
2026-01-30 16:07:26 -0300

    Add Dockerfile and standalone output for production

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
     eslint: {
         ignoreDuringBuilds: true,
     },
     typescript: {
         ignoreBuildErrors: true,
     },
+    output: 'standalone', // ← Agregar esta línea
 };
 
 export default nextConfig;
